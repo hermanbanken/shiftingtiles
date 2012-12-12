afa43a58894179b9e102f928717a6b612470809b
Herman Banken <hermanbanken@gmail.com>
2012-12-12 15:57:32 +0100

    Do more with clearing screen and stuff for fullscreen posters

--- shiftingtiles.js
+++ shiftingtiles.js
@@ -1,45 +1,106 @@
 (function( $ ){
 
-  $.fn.shiftingtiles = function(source, options) {
+  $.fn.shiftingtiles = function(images, options) {
   	
-  	var options = $.extend({
-	  		dummy_prop: function(){},
-	  		dummy_prop2: true,
-	  		photosource: source
-	  	}, options), 
-  		where = this;
+    // Variables
+    var options = $.extend({
+          dummy_prop: function(){},
+          dummy_prop2: true,
+          photosource: images
+        }, options), 
+        timeout, 
+        where = this;
+    
+    where.on("webkitAnimationEnd", ".leave > .row", function(){
+      $(this).parent().addClass("left").removeClass("leave");
+    });
+    where.on("webkitAnimationEnd", ".disappear", function(){
+      console.log("Animation end, removing node",this);
+      // $(this).  this wasn't working, why? no idea
+      $(this).css("display", "none").remove();
+      where.trigger("animate-after");
+
+      return false;
+    });
+
+    setup(this);
+
+    // Source
+    function source(){
+      if(typeof images.bottom == "undefined")
+        images.bottom = 0;
+
+      var index = images.bottom + Math.floor((images.length - images.bottom) * Math.random());
+      console.log(index, 1, images.bottom, images);
+      var one = images.splice(index, 1)[0];
+      images.unshift(one);
+
+      images.bottom++;
+
+      if(images.bottom == images.length){
+        images.bottom = 0;
+        where.trigger("galleryrestart");
+      }
+
+      return one;
+    }
 
   	// Setup
   	function setup(where){
-  	  	where.addClass("shiftingtiles");
+  	 	where.addClass("shiftingtiles");
 	  	where.append("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
 	  	where.append("<div class='row'><div class='single'></div><div class='single'></div><div class='dual'><div></div><div></div></div></div>");
-  	}
+      where.append("<div class='loading'>Loading Photos...</div>");
 
-  	where.on("webkitAnimationEnd", ".disappear", function(){
-  		$(this).css("display", "none").remove();
-  	});
+      where.find(".single, .dual > div").each(addImage);
+      timeout = setTimeout(frame, 5000);
+    }
 
-  	var timeout;
+    // Add background image from source to jQuery element
+    function image($element){
+      $element.css("background-image", "url("+source()+")");
+    }
+
+    // Figure out single or dual and add images
+    function addImage(index, node){
+      node = $(node);
+      console.log(node);
+      // Load new images
+      if(node.hasClass("single") || node.parent(".dual").size() > 0){
+        image(node);
+      } else if(node.hasClass("dual")) {
+        node.children().each(function(){
+          image($(this));
+        });
+      }
+
+      return node;
+    }
+
+    // Animate frame, remove box and add new one
   	function frame(){
   		clearTimeout(timeout);
   		var boxes = where.find(".single:not(:last-child), .dual:not(:last-child)");
   		var disappear = $(boxes.get( ~~(Math.random() * boxes.size()) ));
 
-  		disappear.parent().append(disappear.clone());
+      where.trigger("animate-before", disappear);
+
+  		disappear.parent().append(addImage(0, disappear.clone()));
   		disappear.addClass("disappear");
-	  	timeout = setTimeout(frame, 5000);
+      where.trigger("animate", disappear);
+
+      timeout = setTimeout(frame, 5000);
   	}
-  	timeout = setTimeout(frame, 5000);
+
   	$(document.body).keyup(function(e){
+      console.log("Key up");
   		if(e.keyCode == 32) frame();
-  	})
-
-  	setup(this);
-
-  	// Chainability
+      if(e.keyCode == 38) $(".shiftingtiles").toggleClass("leave");
+  	});
+    
+    // Chainability
     return this;
-  
+
   };
 })( jQuery );
 
